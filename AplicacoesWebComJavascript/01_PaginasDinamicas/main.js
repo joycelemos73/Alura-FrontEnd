9c88b72fcb046ecdc73ca6697530abed70311f56
joycelemos <joyce.lemoss@gmail.com>
2023-04-27 12:04:27 -0300

    :zap: Added sounds to all buttons

--- AplicacoesWebComJavascript/01_PaginasDinamicas/main.js
+++ AplicacoesWebComJavascript/01_PaginasDinamicas/main.js
@@ -1,5 +1,19 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let i = 0;
+while (i < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    i++;
+}
