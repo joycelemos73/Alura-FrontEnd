3e343b64f8aeea82af78cb4d3cdfc562830de04e
joycelemos <joyce.lemoss@gmail.com>
2023-04-27 15:02:16 -0300

    :art: Improved structure

--- AplicacoesWebComJavascript/01_PaginasDinamicas/main.js
+++ AplicacoesWebComJavascript/01_PaginasDinamicas/main.js
@@ -1,11 +1,17 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio'){
+        elemento.play();    
+    } else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let i = 0;
-while (i < listaDeTeclas.length) {
+for ( let i = 0; i < listaDeTeclas.length; i++) {
 
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
@@ -15,5 +21,16 @@ while (i < listaDeTeclas.length) {
         tocaSom(idAudio);
     }
 
-    i++;
+    tecla.onkeydown = function(evento){
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa'); 
+        }
+
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');    
+    }
+
 }
