d1edc81a1067fb21ff850d08167cff9bcb027fa3
joycelemos <joyce.lemoss@gmail.com>
2023-05-03 10:10:05 -0300

    :sparkles: New feature:  Added EventListener to increase and decrease robot skills

---
+++ AplicacoesWebComJavascript/02_ManipulandoDOM/js/main.js
@@ -0,0 +1,23 @@
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+const controle = document.querySelectorAll('.controle-ajuste');
+
+somar.addEventListener("click", () => {
+    manipulaDados("somar");
+});
+
+subtrair.addEventListener("click", () => {
+    manipulaDados("subtrair");
+});
+
+function manipulaDados(operacao) {
+    if (operacao === "somar") {
+        braco.value = (parseInt(braco.value)) + 1;
+    } else {
+        if (braco.value > 0) {
+            braco.value = (parseInt(braco.value)) - 1;
+        }
+    }
+}
