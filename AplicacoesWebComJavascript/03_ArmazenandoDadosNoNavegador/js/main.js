7f2fb74f26bbaf6a650d040346cabe41f9196c56
joycelemos <joyce.lemoss@gmail.com>
2023-05-05 16:04:31 -0300

    :sparkles: New feature: Delete item option available

--- AplicacoesWebComJavascript/03_ArmazenandoDadosNoNavegador/js/main.js
+++ AplicacoesWebComJavascript/03_ArmazenandoDadosNoNavegador/js/main.js
@@ -22,10 +22,10 @@ form.addEventListener("submit", (evento) => {
     if(existe) {
         itemAtual.id = existe.id;
         atualizaElementos(itemAtual);
-        itens[existe.id] = itemAtual;
+        itens[itens.findIndex(elemento => elemento.id)] = itemAtual;
 
     } else {
-        itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
         criaElementos(itemAtual);
         itens.push(itemAtual);
     }
@@ -47,10 +47,30 @@ function criaElementos(item) {
 
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta(item.id));
+
     lista.appendChild(novoItem);
 
 }
 
 function atualizaElementos(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X";
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    })
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+    itens.splice(itens.findIndex(elemento => elemento.id == id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
