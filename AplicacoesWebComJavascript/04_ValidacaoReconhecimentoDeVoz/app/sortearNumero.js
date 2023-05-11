0ac52f9fc604abeaf7d46e1cc835852a238edf42
joycelemos <joyce.lemoss@gmail.com>
2023-05-11 19:11:05 -0300

    :zap: Voice recognition: Added JS file with voice recognition API to HTML

--- AplicacoesWebComJavascript/04_ValidacaoReconhecimentoDeVoz/app/sortearNumero.js
+++ AplicacoesWebComJavascript/04_ValidacaoReconhecimentoDeVoz/app/sortearNumero.js
@@ -1,1 +1,15 @@
-console.log('estou aqui')
+const menorValor = 10;
+const maiorValor = 30;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor+1);
+}
+
+console.log('Numero secreto:', numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor;
