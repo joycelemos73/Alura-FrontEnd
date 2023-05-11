75228273567a4b07a7b0d00b657a55ba5d679092
joycelemos <joyce.lemoss@gmail.com>
2023-05-11 19:51:41 -0300

    :zap: Update: Validation messages are now being shown on screen

--- AplicacoesWebComJavascript/04_ValidacaoReconhecimentoDeVoz/app/validacao.js
+++ AplicacoesWebComJavascript/04_ValidacaoReconhecimentoDeVoz/app/validacao.js
@@ -2,12 +2,19 @@ function verificaSeOChutePossuiValorValido(chute) {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        console.log('Chute inválido: Precisa ser número');
+        elementoChute.innerHTML += '<div>Chute inválido: Precisa ser número</div>';
     }
 
     if(numeroForMaiorOuMenorQueValorPermitido(numero)) {
-        console.log(`Chute inválido: O número secreto precisa estar 
-        entre ${menorValor} e ${maiorValor}`)
+        elementoChute.innerHTML += `<div>Chute inválido: Fale um número 
+        entre ${menorValor} e ${maiorValor}</div>`
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
     }
 }
 
