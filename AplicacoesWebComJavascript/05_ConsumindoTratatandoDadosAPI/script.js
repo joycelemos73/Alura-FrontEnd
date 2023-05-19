770f9d553ad778c5f28d80aaadff1eba969da1cc
joycelemos <joyce.lemoss@gmail.com>
2023-05-19 16:26:16 -0300

    :sparkles: New Feature: Enabled automated form population based on zip code input

--- AplicacoesWebComJavascript/05_ConsumindoTratatandoDadosAPI/script.js
+++ AplicacoesWebComJavascript/05_ConsumindoTratatandoDadosAPI/script.js
@@ -5,9 +5,23 @@ async function buscaEndereco(cep) {
         if (consultaCepConvertida.erro){
             throw Error('CEP não existente!');
         }
+
+        var logradouro = document.getElementById('endereco');
+        var bairro = document.getElementById('bairro');
+        var cidade = document.getElementById('cidade');
+        var estado = document.getElementById('estado');
+
+        logradouro.value = consultaCepConvertida.logradouro;
+        bairro.value = consultaCepConvertida.bairro;
+        cidade.value = consultaCepConvertida.localidade;
+        estado.value = consultaCepConvertida.uf;
+
         console.log(consultaCepConvertida);
         return consultaCepConvertida;
     } catch (erro) {
         console.log(erro)
     }
 }
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
