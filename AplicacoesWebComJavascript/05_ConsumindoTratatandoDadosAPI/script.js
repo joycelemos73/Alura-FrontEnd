bc920381745fc63d5e37d165fd1289ee52ee63d5
joycelemos <joyce.lemoss@gmail.com>
2023-05-19 15:06:59 -0300

    :hammer: Refact: Used assync function to improve code

--- AplicacoesWebComJavascript/05_ConsumindoTratatandoDadosAPI/script.js
+++ AplicacoesWebComJavascript/05_ConsumindoTratatandoDadosAPI/script.js
@@ -1,12 +1,7 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-        throw Error('Esse CEP não existe!')
-    }else 
-        console.log(r)
-    })
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluído'))
+async function buscaEndereco() {
+    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
+    var consultaCepConvertida = await consultaCep.json();
+    console.log(consultaCepConvertida);
+}
 
-console.log(consultaCep);
+buscaEndereco();
