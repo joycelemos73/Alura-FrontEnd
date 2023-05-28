921a4bd5eb52c58319b269940cb0ad8cb1710d71
Joyce Lemos <joyce.lemoss@gmail.com>
2023-05-28 19:02:44 -0300

    :sparkles: New Feature: Enable video submission via enviar-video.html page

--- AplicacoesWebComJavascript/07_CriandoRequisicoes/js/criarVideo.js
+++ AplicacoesWebComJavascript/07_CriandoRequisicoes/js/criarVideo.js
@@ -1,12 +1,17 @@
+import{ conectaApi} from "./conectaApi.js";
 const formulario = document.querySelector("[data-formulario]");
 
-function criarVideo(evento) {
+async function criarVideo(evento) {
     evento.preventDefault();
-    
+
     const image = document.querySelector("[data-imagem]").value;
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectaApi.criaVideos(titulo, descricao, url, imagem);
+
+    window.location.href = "../pages/envio-concluido.html";
 }
 
 formulario.addEventListener("submit", evento => criarVideo(evento));
