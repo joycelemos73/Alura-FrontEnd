8dc0fbf9f5d2332bcf97adc7396a7092b5a5a19d
Joyce Lemos <joyce.lemoss@gmail.com>
2023-05-28 16:58:56 -0300

    :zap: Update: Implement async function to connect to API and add new entries to db.json

--- AplicacoesWebComJavascript/07_CriandoRequisicoes/js/conectaApi.js
+++ AplicacoesWebComJavascript/07_CriandoRequisicoes/js/conectaApi.js
@@ -5,6 +5,24 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
+async function criaVideos(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizacoes`,
+            url: url,
+            imagem: imagem
+        })
+    });
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos, 
+    criaVideos
 }
